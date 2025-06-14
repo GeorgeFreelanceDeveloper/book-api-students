bfabe15c9bb75e29ca79d44af80c06d14f30ffeb
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-06-14 14:07:12 +0200

    improving error JSON answer to more follow rfc7807

--- jankocabek/src/main/java/cz/kocabek/exception/MyGlobalExceptionHandler.java
+++ jankocabek/src/main/java/cz/kocabek/exception/MyGlobalExceptionHandler.java
@@ -1,23 +1,41 @@
 package cz.kocabek.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.time.Instant;
 
 @RestControllerAdvice
-
 public class MyGlobalExceptionHandler {
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(BookNotFoundException.class)
     public ErrorResponse handleBookNotFoundException(BookNotFoundException ex) {
-        return ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
+        final var problemDetail = buildProblemDetail(HttpStatus.NOT_FOUND, ex, "Book not found", "book-not-found");
+        return ErrorResponse.builder(ex, problemDetail).build();
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DuplicatedRecordException.class)
     public ErrorResponse handleDuplicatedRecordException(DuplicatedRecordException ex) {
-        return ErrorResponse.create(ex, HttpStatus.CONFLICT, ex.getMessage());
+        final var problemDetail = buildProblemDetail(HttpStatus.CONFLICT, ex, "Book Duplication", "book-duplication");
+        return ErrorResponse.builder(ex, problemDetail).build();
     }
+
+    private static ProblemDetail buildProblemDetail(HttpStatus status, Exception ex, String title, String type) {
+        final var problemDetail = ProblemDetail.forStatusAndDetail(status, ex.getMessage());
+        final var errPath = "/books/error";
+        final var uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(errPath).toUriString();
+        problemDetail.setTitle(title);
+        problemDetail.setType(URI.create(uri + "/" + type));
+        problemDetail.setProperty("timestamp", Instant.now().toString());
+        return problemDetail;
+    }
+
 }
