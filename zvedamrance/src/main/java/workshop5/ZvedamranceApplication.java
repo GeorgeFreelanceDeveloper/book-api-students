d185234db1f05a9c016f0544ae8c544def0c0dbd
ZvedamRance <Dolisko@seznam.cz>
2025-06-15 11:54:11 +0200

    workshop5

---
+++ zvedamrance/src/main/java/workshop5/ZvedamranceApplication.java
@@ -0,0 +1,13 @@
+package workshop5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZvedamranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZvedamranceApplication.class, args);
+	}
+
+}
